902c04d7b491cabcfaaab66619a6cc3fcf87a5f1
Payne Richards <payne.a.richards@gmail.com>
2017-08-10 09:52:50 -0500

    first commit

---
+++ Assets/Scripts/GroundController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	private float rotx;
+	private float roty;
+	private float rotz;
+	private float rotw;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rotx = 0;
+		roty = 0;
+		rotz = 0;
+		rotw = 0;
+
+//		Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+		if (!Input.gyro.enabled) 
+		{
+			Input.gyro.enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+//		rotx = Input.acceleration.normalized.x;
+//		roty = Input.acceleration.normalized.y;
+//		rotz = Input.acceleration.normalized.z;
+//		rb.transform.rotation = new Quaternion ((rotx * 0.001f), (roty * 0.001f), (rotz * 0.001f), (rotw * 0.001f));
+//		
+		Quaternion gyro2 = Input.gyro.attitude; 
+
+		rb.transform.rotation = new Quaternion ( gyro2.x, -gyro2.z, gyro2.y, -gyro2.w);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+//		transform.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("PickUp")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 13) 
+		{
+			winText.text = "You Win";
+		}
+	}
+}
